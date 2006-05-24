b7647699a24c4b715439241da4452fcd8375a54b
zebarjan <zebarjan@ncias-d1611-v.nci.nih.gov>
2006-05-24 14:31:58 +0000

    Moved demo-specific files to demo directory

--- caCORE.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem OLD caCORE Ant task
-rem @ant -f core-build.xml %1 %2 %3
-
-
-ant -f ..\cacoresdk\build.xml -Dfile.build.custom=..\caCORESYSTEM\build-custom-cacore.xml -Dfile.properties.custom=..\caCORESYSTEM\build-custom-cacore.properties %1 %2 %3
-
