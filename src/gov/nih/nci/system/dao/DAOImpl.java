efd0d9a3fe8ea1e4fa28b9a1c490aa722b9fd7b9
shaziyam <shaziyam@ncias-d1611-v.nci.nih.gov>
2006-07-17 19:16:53 +0000

    Implements DAOImpl interface

---
+++ src/gov/nih/nci/system/dao/DAOImpl.java
@@ -0,0 +1,10 @@
+package gov.nih.nci.system.dao;
+/*
+ * Created on Jun 27, 2006
+ * ShaziyaMuhsin
+ * 
+ */
+import gov.nih.nci.common.net.*;
+public interface DAOImpl{
+    public Response query(Request request) throws Exception;
+}
