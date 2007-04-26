c3419d396f3248d89ce96fb686fd8284764b2ec6
shaziyam <shaziyam@ncias-d1611-v.nci.nih.gov>
2007-04-26 19:47:31 +0000

    GF5979:Creates indexes

--- src/gov/nih/nci/system/dao/impl/search/utils/Indexer.java
+++ src/gov/nih/nci/system/dao/impl/search/utils/Indexer.java
@@ -57,7 +57,7 @@ public class Indexer extends Thread{
                 breakPoint = 0;
                 fullTextSession.clear();                
             }
-            counter++;
+            breakPoint++;
         }
         long end = System.currentTimeMillis();
         results.last();        
